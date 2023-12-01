141ae27c766d2f2cae169ffabf3a24d75a9a99ea
NikhilTekale7 <deshmukhclass12@gmail.com>
2023-12-01 15:57:59 +0530

    added file'
    git status
    
    git commit

---
+++ src/add.java
@@ -0,0 +1,18 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		addition(10, 20);
+		
+	}
+	public  static void addition(int a , int b) {
+		
+		int c= a=+b ;
+		int d= b-a ;
+		System.out.println("value of c>>"+c);
+		System.out.println("value of d>>"+d);
+	}
+
+}
+
+
